44278b01a77caceac0b39010cb345cfc1e3e3469
Sofian Audry <info@sofianaudry.com>
2011-08-31 12:04:17 -0400

    Added cpp_include.h + a few fixes

--- main_test.cpp
+++ main_test.cpp
@@ -1,3 +1,5 @@
+#include "cpp_include.h"
+
 #include "Qualia.h"
 #include "DummyAgent.h"
 #include "DummyEnvironment.h"
@@ -20,7 +22,7 @@ void testDummy() {
   for (int i=0; i<10; i++) {
     qualia.episode(10);
     printf("Current agent action: %d\n", agent.currentAction);
-    printf("Current environment observation: %f\n", env.currentObservation);
+    printf("Current environment observation: %f\n", (double)env.currentObservation);
   }
 }
 
@@ -34,15 +36,15 @@ void testQLearning() {
   qualia.start();
   for (int i=0; i<1000; i++) {
     qualia.step();
-    printf("Mean reward: %f\n", qualia.totalReward / qualia.nSteps);
+    printf("Mean reward: %f\n", (double) qualia.totalReward / qualia.nSteps);
 //    printf("Current agent action: %d\n", agent._nextAction);
 //    printf("Current environment observation: %f\n", env.currentObservation);
   }
 
 }
 
-unsigned char buffer[200];
-StaticAllocator myAlloc(buffer, 100);
+unsigned char buffer[STATIC_ALLOCATOR_SIZE];
+StaticAllocator myAlloc(buffer, STATIC_ALLOCATOR_SIZE);
 int main() {
   Alloc::init(&myAlloc);
 //  testDummy();
