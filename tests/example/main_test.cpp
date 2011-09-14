15075723196d760ceafc20708e74a7c94cb8bc22
Sofian Audry <info@sofianaudry.com>
2011-09-14 12:19:16 -0400

    Moved main_test to examples

--- tests/example/main_test.cpp
+++ tests/example/main_test.cpp
@@ -10,8 +10,6 @@
 #define STATIC_ALLOCATOR_SIZE 100
 #include "StaticAllocator.h"
 
-#include <stdio.h>
-
 void testDummy() {
   DummyAgent agent;
   DummyEnvironment env;
@@ -21,8 +19,8 @@ void testDummy() {
 
   for (int i=0; i<10; i++) {
     qualia.episode(10);
-    printf("Current agent action: %d\n", agent.currentAction);
-    printf("Current environment observation: %f\n", (double)env.currentObservation);
+//    printf("Current agent action: %d\n", agent.currentAction);
+//    printf("Current environment observation: %f\n", (double)env.currentObservation);
   }
 }
 
@@ -36,7 +34,7 @@ void testQLearning() {
   qualia.start();
   for (int i=0; i<1000; i++) {
     qualia.step();
-    printf("Mean reward: %f\n", (double) qualia.totalReward / qualia.nSteps);
+//    printf("Mean reward: %f\n", (double) qualia.totalReward / qualia.nSteps);
 //    printf("Current agent action: %d\n", agent._nextAction);
 //    printf("Current environment observation: %f\n", env.currentObservation);
   }
