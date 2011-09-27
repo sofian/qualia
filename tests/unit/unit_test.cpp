ce6ea4cb2374f995e242d77ce3dad3d49d73ac6b
Sofian Audry <info@sofianaudry.com>
2011-09-27 18:24:33 -0400

    Unit testing

---
+++ tests/unit/unit_test.cpp
@@ -0,0 +1,32 @@
+/*
+ * unit_test.cpp
+ *
+ * (c) 2011 Sofian Audry -- info(@)sofianaudry(.)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "Action.h"
+
+const unsigned int nActions[] = {3, 2, 2};
+void testActions() {
+  Action test(3, nActions);
+//  tests.nActions[0] = 3;
+
+}
+
+int main() {
+
+}
