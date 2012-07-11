b0d6da61c03771ac9d48a3baf908cf5b50a4fb8a
Tats <info@sofianaudry.com>
2012-07-11 13:02:43 +0200

    Use the new methods from random.h and treat the case epsilon = 1 or = 0

--- src/rl/QLearningSoftmaxPolicy.cpp
+++ src/rl/QLearningSoftmaxPolicy.cpp
@@ -33,6 +33,9 @@ void QLearningSoftmaxPolicy::chooseAction(Action* action, const Observation* obs
   // TODO: gerer les cas epsilon == 0 et epsilon == 1
   if (Random::uniform() < epsilon)
     action->setConflated( (action_t) (random() % qlagent->nConflatedActions) ); // TODO: changer le % _nActions pour une fonction random(min, max)
+  if (epsilon >= 1 ||
+      (epsilon > 0 && randomUniform() < epsilon))
+    action->setConflated( (action_t) random(qlagent->nConflatedActions) ); // TODO: changer le % _nActions pour une fonction random(min, max)
   else {
     // First pass: compute (1/n) * sum_a exp[ Q(s,a) / T ] / exp[ Q_max(s,a) / T ]
     // Dividing each element by the max is designed to avoid overflows
@@ -56,6 +59,7 @@ void QLearningSoftmaxPolicy::chooseAction(Action* action, const Observation* obs
 
     // Second pass: pick a random action according to the softmax distribution.
     real rnd = Random::uniform() * sum;
+    real rnd = randomUniform(sum);
     action->reset();
     while (action->hasNext()) {
       real out = exp( qlagent->Q(observation, action) / temperature ) / outMax / n;
