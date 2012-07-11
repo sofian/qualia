fb4839234be7b82a3367dca13ed3bf70bcd9ec09
Tats <info@sofianaudry.com>
2012-07-11 11:45:12 +0200

    Begin refactoring the random generators: added Arduino methods and removed the static Random class

--- src/util/Random.h
+++ src/util/Random.h
@@ -29,118 +29,28 @@
 #ifndef RANDOM_INC
 #define RANDOM_INC
 
+#include "common.h"
+
 #include <limits.h>
 #include <stdlib.h>
-#include <math.h>
 
-//#include "WProgram.h"
-
-class Random {
-
-public:
-  /// Initializes the random number generator with the given long "the_seed_".
-  //  static void manualSeed(unsigned long the_seed_) {
-  //    randomSeed(the_seed_);
-  //  }
-
-  /// Initializes the random number generator by using analogRead on an open pin.
-  /// XXX not tested yet!
-  //  static void seed(byte pin) {
-  //    unsigned long seed = 0;
-  //    for (int s=0; s<32; s+=10) {
-  //      unsigned long val = 0;
-  //      do {
-  //        val = analogRead(pin);
-  //      } while (val == 0); // discard zeros
-  //      seed |= (val << s);
-  //    }
-  //    randomSeed( seed );
-  //  }
-
-  /// Generates a uniform 32 bits integer.
-  //  static unsigned long random() {
-  //    return ::random();
-  //  }
-
-  /// Generates a uniform random number on [0,1[.
-  static float uniform() {
-    return (float)random() / (float)
-#ifdef RANDOM_MAX
-        RANDOM_MAX
-#else
-        RAND_MAX
+#if defined(__cplusplus)
+extern "C" {
 #endif
-        ;
-  }
 
-  /// Returns in #indices# #n_indices# shuffled. (between 0 and #n_indices-1#).
-  static void getShuffledIndices(int *indices, int n_indices) {
-    for (int i=0; i<n_indices; i++) {
-      int z = random() % (n_indices - i);
-      // swap indices[i] with indices[i+z]
-      int tmp = indices[i];
-      indices[i] = indices[i+z];
-      indices[i+z] = tmp;
-    }
-  }
+#if !is_arduino()
+// Code from Arduino WMath.cpp
+void randomSeed(unsigned int seed);
+long random(long howbig);
+long random(long howsmall, long howbig);
+#endif
 
-  /// Generates a uniform random number on [a,b[ (b>a).
-  static float boundedUniform(float a, float b) {
-    return(Random::uniform() * (b - a) + a);
-  }
+/// Generates a uniform random number on [0,1[.
+float randomUniform();
+float randomUniform(float a, float b);
 
-  /** Generates a random number from a normal distribution.
-      (With mean #mean# and standard deviation #stdv >= 0#).
-   */
-  static float normal(float mean=0, float stdv=1) {
-    static bool normal_is_valid = false;
-    static float normal_x;
-    static float normal_y;
-    static float normal_rho;
-    if(!normal_is_valid)
-    {
-      normal_x = Random::uniform();
-      normal_y = Random::uniform();
-      normal_rho = sqrt(-2. * log(1.0-normal_y));
-      normal_is_valid = true;
-    }
-    else
-      normal_is_valid = false;
-
-    if(normal_is_valid)
-      return normal_rho*cos(2.*M_PI*normal_x)*stdv+mean;
-    else
-      return normal_rho*sin(2.*M_PI*normal_x)*stdv+mean;
-  }
-
-  /** Generates a random number from an exponential distribution.
-      The density is $p(x) = lambda * exp(-lambda * x)$, where
-      lambda is a positive number.
-   */
-  //  static float exponential(float lambda) {
-  //    return(-1. / lambda * log(1-Random::uniform()));
-  //  }
-
-  /** Returns a random number from a Cauchy distribution.
-      The Cauchy density is $p(x) = sigma/(pi*(sigma^2 + (x-median)^2))$
-   */
-  //  static float cauchy(float median=0, float sigma=1) {
-  //    return(median + sigma * tan(M_PI*(Random::uniform()-0.5)));
-  //  }
-
-  /** Generates a random number from a geometric distribution.
-      It returns an integer #i#, where $p(i) = (1-p) * p^(i-1)$.
-      p must satisfy $0 < p < 1$.
-   */
-  //  static int geometric(float p) {
-  //    return((int)(log(1-Random::uniform()) / log(p)) + 1);
-  //  }
-
-  /// Returns true with probability $p$ and false with probability $1-p$ (p > 0).
-  //  static bool bernouilli(float p=0.5) {
-  //    return(Random::uniform() <= p);
-  //  }
-
-};
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
