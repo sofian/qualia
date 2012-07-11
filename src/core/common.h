3d7d9815d6035fb0bdb88090272fa2644f8d0448
Tats <info@sofianaudry.com>
2012-07-11 10:16:46 +0200

    Cleanup comments

--- src/core/common.h
+++ src/core/common.h
@@ -71,42 +71,4 @@ typedef float real;
 #include "Action.h"
 #include "Observation.h"
 
-
-//typedef real* observation_t;
-//class observation_t {
-//public:
-//  real* continuous;
-//  int*  discrete;
-//
-//  observation_t(int nContinuous, int nDiscrete) {
-//    continous = malloc(nContinuous * sizeof(real));
-//    discrete  = malloc(nDiscrete   * sizeof(int) );
-//  }
-//  ~observation_t() {
-//    free(continuous);
-//    free(discrete);
-//  }
-//};
-
-// TODO: should eventually be an array or struct
-//typedef int action_t;
-
-//typedef struct {
-//  observation_t observation;
-//  action_t action;
-//} observation_action_t;
-//
-//typedef struct {
-//  real reward;
-//  observation_t observation;
-//  int terminal;
-//} reward_observation_terminal_t;
-//
-//typedef struct {
-//  real reward;
-//  observation_t observation;
-//  action_t action;
-//  int terminal;
-//} reward_observation_action_terminal_t;
-
 #endif
