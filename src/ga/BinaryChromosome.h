5d13c02dc80a9caf669e4f77c39f6630fdd880ca
Tats <info@sofianaudry.com>
2012-07-17 21:37:30 +0200

    Cleanup the code

--- src/ga/BinaryChromosome.h
+++ src/ga/BinaryChromosome.h
@@ -23,79 +23,41 @@
 #include "Chromosome.h"
 #include <qualia/util/bits.h>
 
-typedef float (*Evaluator) (Chromosome &);
-typedef void  (*Initializer) (Chromosome &);
-typedef void  (*Mutator) (Chromosome &, float);
-typedef float (*Comparator) (const Chromosome&, const Chromosome&);
-typedef void  (*SexualCrossover) (const Chromosome&, const Chromosome&,
-                                  Chromosome*, Chromosome*);
-typedef void  (*AsexualCrossover) (const Chromosome&, Chromosome*);
+//void defaultMutator(Chromosome& chromosome, float);
+//void defaultInitializer(Chromosome& chromosome);
 
 class BinaryChromosomeInfo {
 public:
-  int nGenes;
+  unsigned int nGenes;
   uint8_t* geneSizes; // the size (in bits) of each gene
-  int codeSize;
+  //int codeSize;
 
   Initializer initializer;
   Mutator mutator;
   Evaluator evaluator;
 
-  BinaryChromosomeInfo(Initializer initializer, Mutator mutator, Evaluator evaluator);
+  BinaryChromosomeInfo(unsigned int nGenes, const uint8_t* geneSizesInit=0,
+                       Initializer initializer=0, Mutator mutator=0);
 
-  int codeSize() const;
+  unsigned int bitSize() const;
+  unsigned int byteSize() const { return ( (bitSize()+7) / 8); }
   int getStartBitPosition(int gene) const;
+
+  void allocate(unsigned int nGenes, const uint8_t* geneSizesInit=0);
 };
-
-void RightShiftMemCopy(uint8_t * pSource, uint8_t * pDest ,ushort len,uint8_t shiftOffset)
-{
-    ushort i=0;
-
-    pDest+=(len-1);
-    pSource+=(len-1);
-
-    for(i=len-1;i != 0 ;--i)
-    {
-        *pDest = (*(pSource - 1) << 8 | *pSource) >> shiftOffset;
-
-        --pDest;
-        --pSource;
-    }
-
-    *pDest = *pSource >> shiftOffset;
-
-}
-
-void readBits(uint8_t* dst, const uint8_t* src, int pos, int length, int nBytesDst) {
-  uint8_t shiftOffset = pos % 8;
-
-
-  dst += (length-1);
-  src += (length-1);
-
-  for(int i=length-1; i != 0 ; --i)
-  {
-      *dst = (*(src - 1) << 8 | *src) >> shiftOffset;
-
-      --dst;
-      --src;
-  }
-
-  *dst = *src >> shiftOffset;
-}
 
 class BinaryChromosome: public Chromosome {
 public:
   BinaryChromosomeInfo* info;
   uint8_t* code;
 
-  BinaryChromosome(int nGenes, BinaryChromosomeInfo* info);
+  BinaryChromosome(BinaryChromosomeInfo* info);
   virtual ~BinaryChromosome();
 
   virtual void init();
   virtual void mutate(float p);
-  virtual int compare(const Chromosome& g);
-  virtual float evaluate();
+//  virtual int compare(const Chromosome& g);
+//  virtual float evaluate();
 
   double doubleValue(int gene);
   int    intValue(int gene);
