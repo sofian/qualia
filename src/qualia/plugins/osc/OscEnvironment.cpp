8f1b3da0cb4faae2c5ce1de13fb07d16ee999981
Tats <info@sofianaudry.com>
2012-12-19 13:13:10 -0500

    Added a new environment for OSC (liblo)

---
+++ src/qualia/plugins/osc/OscEnvironment.cpp
@@ -0,0 +1,173 @@
+/*
+ * OscEnvironment.cpp
+ *
+ * (c) 2012 Sofian Audry -- info(@)sofianaudry(.)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "OscEnvironment.h"
+
+OscEnvironment::OscEnvironment(int observationDim_, int actionDim_)
+  : id(-1), observationDim(observationDim_), actionDim(actionDim_), locked(false) {
+  observationBuffer = (float*)malloc(observationDim*sizeof(float));
+  repeatChar(observationTypes, 'f', observationDim);
+  repeatChar(actionTypes, 'i', actionDim);
+}
+
+OscEnvironment::~OscEnvironment() {
+}
+
+void OscEnvironment::initOsc(const char* host, const char* port, const char* remotePort) {
+  client = lo_address_new(host, remotePort);
+
+  /* start a new server on port 7770 */
+  server = lo_server_thread_new(port, OscEnvironment::error);
+
+  lo_server_thread_add_method(server, "/qualia/response/create", 0, OscEnvironment::handlerCreate, 0);
+
+  /* add method that will match any path and args */
+  // lo_server_thread_add_method(server, "/bang", "", OscManager::bangHandler, 0);
+  lo_server_thread_start(server);
+}
+
+void OscEnvironment::createEnvironmentId() {
+  static int envId;
+  envId = -1;
+  lo_send(client, "/qualia/create", 0, &envId);
+  while (envId == -1) usleep(1000);
+}
+
+char* OscEnvironment::getPath(const char* path) {
+  static char envPath[100];
+  sprintf(envPath, "%s/%d", path, id);
+  return envPath;
+}
+
+void OscEnvironment::init() {
+  if (client == 0) {
+    printf("Error: OscEnvironment::initOsc() should be called prior to calling init()\n");
+    exit(-1);
+  }
+
+  // Init.
+  if (id == -1) {
+    // Get id from OSC.
+    OscEnvironment::createEnvironmentId();
+    assert(id != -1);
+
+    // Create methods for responses.
+    lo_server_thread_add_method(server, getPath("/qualia/response/init"), 0, OscEnvironment::handlerInit, this);
+    lo_server_thread_add_method(server, getPath("/qualia/response/start"), observationTypes, OscEnvironment::handlerStartStep, this);
+    lo_server_thread_add_method(server, getPath("/qualia/response/step"), observationTypes, OscEnvironment::handlerStartStep, this);
+  }
+
+  locked = true;
+  lo_send(client, getPath("/qualia/init"), 0);
+  while (locked) usleep(1000);
+  printf("%d: init() done\n", id);
+}
+
+Observation* OscEnvironment::start() {
+  // Lock.
+  locked = true;
+
+  // Send message.
+  lo_send(client, getPath("/qualia/start"), 0);
+
+  // Wait for response.
+  while (locked) usleep(1000);
+  printf("%d: start() done\n", id);
+
+  // Return observation.
+  return readObservation(observationBuffer);
+}
+
+Observation* OscEnvironment::step(const Action* action) {
+  // Lock.
+  locked = true;
+
+  // Send message.
+  lo_message msg = lo_message_new();
+  for (unsigned int i=0; i<action->dim; i++)
+    if (sizeof(action_t) <= 32)
+      lo_message_add_int32(msg, action->actions[i]);
+    else
+      lo_message_add_int64(msg, action->actions[i]);
+  lo_send_message(client, getPath("/qualia/step"), msg);
+  lo_message_free(msg);
+
+  // Wait for response.
+  while (locked) usleep(1000);
+  printf("%d: step() done\n", id);
+
+  // Return observation.
+  return readObservation(observationBuffer);
+}
+
+void OscEnvironment::repeatChar(char* dst, char c, int times) {
+  memset(dst, c, times);
+  dst[times] = '\0';
+}
+
+int OscEnvironment::handlerCreate(const char *path, const char *types, lo_arg **argv,
+                                  int argc, void *data, void *user_data) {
+  *((int*)user_data) = argv[0]->i;
+  printf("Creation accepted: id = %d\n", argv[0]->i);
+  return 0;
+}
+
+int OscEnvironment::handlerInit(const char *path, const char *types, lo_arg **argv,
+                                int argc, void *data, void *user_data) {
+  OscEnvironment *obj = ((OscEnvironment*)user_data);
+  obj->locked = false;
+  return 0;
+}
+
+int OscEnvironment::handlerStartStep(const char *path, const char *types, lo_arg **argv,
+                                     int argc, void *data, void *user_data) {
+  OscEnvironment *obj = ((OscEnvironment*)user_data);
+  assert(argc == obj->observationDim);
+  for (int i=0; i<argc; i++) {
+    float x;
+    switch (types[i]) {
+    case LO_INT32:
+      x = (float)argv[i]->i;
+      break;
+    case LO_INT64:
+      x = (float)argv[i]->h;
+      break;
+    case LO_FLOAT:
+      x = argv[i]->f;
+      break;
+    case LO_DOUBLE:
+      x = (float)argv[i]->d;
+      break;
+    default:
+      printf("Error: wrong type at index %d: %c.\n", i, types[i]);
+      exit(-1);
+    }
+
+    // Copy to buffer.
+    obj->observationBuffer[i] = x;
+  }
+  obj->locked = false;
+  return 0;
+}
+
+void OscEnvironment::error(int num, const char *msg, const char *path)
+{
+    printf("liblo server error %d in path %s: %s\n", num, path, msg);
+    fflush(stdout);
+}
---
+++ src/qualia/plugins/osc/OscEnvironment.h
@@ -0,0 +1,74 @@
+/*
+ * OscEnvironment.h
+ *
+ * (c) 2012 Sofian Audry -- info(@)sofianaudry(.)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OSCENVIRONMENT_H_
+#define OSCENVIRONMENT_H_
+
+#include <qualia/core/Environment.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <lo/lo.h>
+
+class OscEnvironment : public Environment {
+public:
+  static lo_address client;
+  static lo_server_thread server;
+
+  int id;
+
+  int observationDim;
+  int actionDim;
+  float* observationBuffer;
+  volatile bool locked;
+
+  char observationTypes[100];
+  char actionTypes[100];
+
+public:
+  OscEnvironment(int observationDim, int actionDim);
+  virtual ~OscEnvironment();
+
+  static void initOsc(const char* host, const char* port, const char* remotePort);
+  static void createEnvironmentId();
+
+  char* getPath(const char* path);
+
+  // Main qualia environment methods.
+  virtual void init();
+  virtual Observation* start();
+  virtual Observation* step(const Action* action);
+
+  // Read the observation from the observationBuffer.
+  virtual Observation* readObservation(float* buffer) = 0;
+
+  static void repeatChar(char* dst, char c, int times);
+
+  static int handlerCreate(const char *path, const char *types, lo_arg **argv,
+                           int argc, void *data, void *user_data);
+  static int handlerInit(const char *path, const char *types, lo_arg **argv,
+                         int argc, void *data, void *user_data);
+  static int handlerStartStep(const char *path, const char *types, lo_arg **argv,
+                         int argc, void *data, void *user_data);
+  static void error(int num, const char *m, const char *path);
+
+};
+
+#endif /* PROTOTYPE2ENVIRONMENT_H_ */
