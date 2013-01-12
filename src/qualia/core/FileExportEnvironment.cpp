6bb7e1f6604d3ce696b5e230db0f1878fd9e4d6c
Tats <info@sofianaudry.com>
2013-01-12 14:16:28 -0500

    Environment that exports to a file

---
+++ src/qualia/core/FileExportEnvironment.cpp
@@ -0,0 +1,46 @@
+/*
+ * FileExportEnvironment.cpp
+ *
+ * (c) 2013 Sofian Audry -- info(@)sofianaudry(.)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "FileExportEnvironment.h"
+
+FileExportEnvironment::FileExportEnvironment(Environment* env_, XFile* file_)
+  : Environment(), env(env_), file(file_) {
+  ASSERT_ERROR(env);
+  ASSERT_ERROR(file);
+}
+
+FileExportEnvironment::~FileExportEnvironment() {
+}
+
+void FileExportEnvironment::init() {
+  file->rewind();
+}
+
+Observation* FileExportEnvironment::start() {
+  Observation* obs = env->start();
+  obs->saveData(file);
+  return obs;
+}
+
+Observation* FileExportEnvironment::step(const Action* action) {
+  Observation* obs = env->step(action);
+  action->saveData(file);
+  obs->saveData(file);
+  return obs;
+}
