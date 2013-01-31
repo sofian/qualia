9dfaacc30a74ee36953495ae077b1d832eca2cea
Tats <info@sofianaudry.com>
2013-01-31 15:54:10 -0500

    Added an offset agent id to multirun.py

--- tests/osc/multirun.py
+++ tests/osc/multirun.py
@@ -6,21 +6,25 @@ import time
 
 procs = []
 
-if len(sys.argv) < 3:
-  print "Usage: " + sys.argv[0] + " <n_processes> " + " <exec> <exec_args ...>"
+if len(sys.argv) < 4:
+  print "Usage: " + sys.argv[0] + " <start_id> <n_processes>  <exec> <exec_args ...>"
   exit(0)
 
 def stop(sig, frame):
+#  NO NEED TO DO THIS (it seems the CTRL-C is just sent automatically to the forked processes)
 #  for i in range(nProcs):
+#    procs[i].terminate()
 #    procs[i].send_signal(signal.SIGTERM)
-#  time.sleep(2)
+#    procs[i].send_signal(signal.CTRL_C_EVENT)
+  time.sleep(3)
   exit(0)
 
 signal.signal(signal.SIGINT, stop)
 
-nProcs = int(sys.argv[1])
-procPath = sys.argv[2]
-procArgs = sys.argv[3:]
+startId = int(sys.argv[1])
+nProcs = int(sys.argv[2])
+procPath = sys.argv[3]
+procArgs = sys.argv[4:]
 
 for i in range(nProcs):
   procCompleteArgs = [ procPath, str(i) ] + procArgs
