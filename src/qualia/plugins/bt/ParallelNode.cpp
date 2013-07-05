sofian/qualia

commit 0ce78ccf39f9acf7aa4e66f25f74127a4c5dafca

Fixed some warnings in ParallelNode

src/qualia/plugins/bt/ParallelNode.cpp
@@ -92,12 +92,12 @@ BEHAVIOR_STATUS ParallelNode::execute(void* agent)
 			break;
 		}
 	}
-	if (failPolicy == FAIL_ON_ALL && sawAllFails)
+	if (sawAllFails && failPolicy == FAIL_ON_ALL)
 	{
 		init(agent);
 		return BT_FAILURE;
 	}
-	else if (succeedPolicy == SUCCEED_ON_ALL && sawAllSuccess || succeedPolicy == SUCCEED_ON_ONE && sawSuccess)
+	else if ( sawSuccess && (succeedPolicy == SUCCEED_ON_ALL || succeedPolicy == SUCCEED_ON_ONE) )
 	{
 		init(agent);
 		return BT_SUCCESS;

commit 81ca9403c104d5408c0391545575fbba00d4be25

Fix error introduced in commit 0ce78ccf39f9acf7aa4e66f25f74127a4c5dafca

src/qualia/plugins/bt/ParallelNode.cpp
@@ -97,7 +97,7 @@ BEHAVIOR_STATUS ParallelNode::execute(void* agent)
 		init(agent);
 		return BT_FAILURE;
 	}
-	else if ( sawSuccess && (succeedPolicy == SUCCEED_ON_ALL || succeedPolicy == SUCCEED_ON_ONE) )
+	else if ( (sawAllSuccess && succeedPolicy == SUCCEED_ON_ALL) || (sawSuccess && succeedPolicy == SUCCEED_ON_ONE) )
 	{
 		init(agent);
 		return BT_SUCCESS;